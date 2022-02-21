fd65927ddce3c7d4ed16f4e6b061babf75772c13
Dustin Jang <dustin.o.jang@gmail.com>
2022-02-21 15:25:45 -0700

    cleaned up

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,21 +1,10 @@
 import React, { useState } from "react";
 import { AppBar, Toolbar, Typography } from "@mui/material";
-import Account from "./Account";
 import { NavLink } from "react-router-dom";
 import Dustin from "../../assets/Dustin.jpeg";
-
-import Box from "@mui/material/Box";
 import IconButton from "@mui/material/IconButton";
-import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
 import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
-
-const pages = ["Products", "Pricing", "Blog"];
-const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 const Header = () => {
   return (
