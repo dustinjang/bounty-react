5d8f914b15ee5bd7588f2f5e8bbb35d1b100cbff
Dustin Jang <dustin.o.jang@gmail.com>
2022-02-21 20:48:41 -0700

    added comments

--- src/components/bounties/BountyDetail.js
+++ src/components/bounties/BountyDetail.js
@@ -1,7 +1,20 @@
 import React from "react";
+import {
+  useParams,
+  Switch,
+  Route,
+} from "react-router-dom/cjs/react-router-dom.min";
 
 const BountyDetail = () => {
-  return <div>BountyDetail</div>;
+  const params = useParams();
+
+  return (
+    <Switch>
+      <h1>Bounty Details</h1>
+      <p>{params.bountyId}</p>
+      <Route path={`/bounty/${params.bounty}/comments`}></Route>
+    </Switch>
+  );
 };
 
 export default BountyDetail;
